7b3e7589455ca8d7b0ec3c25bc2b8db4ab015329
Hadley Wickham <hadley@rstudio.com>
2020-05-24 14:00:02 +0000

    version 2.3.0

--- R/utils.R
+++ R/utils.R
@@ -18,3 +18,13 @@ is_integerish <- function(x) {
   x_finite <- x[finite_elts & !missing_elts]
   all(x_finite == as.integer(x_finite))
 }
+
+cat_line <- function(...) {
+  cat(paste0(..., "\n", collapse = ""))
+}
+
+# TODO: Remove once vec_cast() preserves names.
+# https://github.com/r-lib/vctrs/issues/623
+vec_cast_named <- function(x, to, ...) {
+  stats::setNames(vec_cast(x, to, ...), names(x))
+}
